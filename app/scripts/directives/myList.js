f6d4498b0a4df5f164b8a144ace5b532b71d339f
viktor992 <viktor992@hotmail.com>
2015-08-15 14:44:37 -0400

    Show modal onDblclick

--- app/scripts/directives/myList.js
+++ app/scripts/directives/myList.js
@@ -7,15 +7,15 @@ angular.module('tareawebApp').
         items: '='
       },
       templateUrl: 'views/myList.html',
-      link: function(scope){
+      link: function(scope,modal){
         scope.modal = {}
         scope.setDetail = function (user, opts) {
           scope.modal.title = user[opts.title];
           scope.modal.body = [];
           for (data in opts.body){
-            console.log(data);
             scope.modal.body.push({label:data,value:user[data]});
           }
+          $('#myModal').modal('show');
         };
       }
     };
