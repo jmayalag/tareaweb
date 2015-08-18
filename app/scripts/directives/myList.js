bf3cc35ae7a2a4786b7f756b89cd58cb1fefe468
Jordan <jayala1993@outlook.com>
2015-08-18 11:03:25 -0400

    Al llamar a cleanFilters, se vuelven a pedir los datos.

--- app/scripts/directives/myList.js
+++ app/scripts/directives/myList.js
@@ -1,42 +1,41 @@
 angular.module('tareawebApp').
-  directive('myList', function() {
+  directive('myList', function () {
     return {
       restrict: 'E',
       scope: {
         options: '=',
-        factoryName : '@'
+        factoryName: '@'
       },
       templateUrl: 'views/myList.html',
-      link: function(scope,element){
+      link: function (scope, element) {
 
         var factoryInstance = element.injector().get(scope.factoryName);
         scope.page = 0;
         scope.pages = 10;
         //scope.items =  factoryInstance.query({page:(scope.page)});
-        scope.searchFields= {_start: 0,_limit:10};
-        scope.items =  factoryInstance.query(scope.searchFields);
+        scope.searchFields = {_start: 0, _limit: 10};
+        scope.items = factoryInstance.query(scope.searchFields);
 
-        scope.doFilter = function(){
+        scope.doFilter = function () {
+          for (var k in scope.searchFields)
+            if (!scope.searchFields[k]) delete scope.searchFields[k];
 
-          for(var k in scope.searchFields)
-            if(!scope.searchFields[k]) delete scope.searchFields[k];
-
-          scope.searchFields['_start']=0;
+          scope.searchFields['_start'] = 0;
           scope.items = factoryInstance.query(scope.searchFields);
         };
 
-        scope.cleanFilters = function(){
-          scope.searchFields= {_start: 0,_limit:10};
+        scope.cleanFilters = function () {
+          scope.searchFields = {_start: 0, _limit: 10};
+          scope.items = factoryInstance.query(scope.searchFields);
         };
 
-        scope.changePage = function(newPage)
-        {
+        scope.changePage = function (newPage) {
           scope.page = newPage;
-          scope.searchFields['_start']=scope.page*10;
+          scope.searchFields['_start'] = scope.page * 10;
           scope.items = factoryInstance.query(scope.searchFields);
         };
 
-        scope.getPages = function() {
+        scope.getPages = function () {
           return new Array(scope.pages);
         };
 
@@ -44,8 +43,8 @@ angular.module('tareawebApp').
         scope.setDetail = function (user, opts) {
           scope.modal.title = user[opts.title];
           scope.modal.body = [];
-          for (data in opts.body){
-            scope.modal.body.push({label:opts.body[data],value:user[data]});
+          for (data in opts.body) {
+            scope.modal.body.push({label: opts.body[data], value: user[data]});
           }
           $('#myModal').modal('show');
         };
