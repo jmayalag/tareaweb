82f4f65f343bd45dcae4592d90c227decf58b78c
Jordan <jayala1993@outlook.com>
2015-08-18 11:04:54 -0400

    Se obtiene el recurso de la api remota pweb-demo. El query ya no retorna un array. Se envian los headers necesarios para el request.

--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -1,8 +1,9 @@
 angular.module('tareawebApp')
   .factory('User', ['$resource', function ($resource) {
 //  return $resource('http://jsonplaceholder.typicode.com/:userId', {}, {
-//    return $resource('http://localhost:3000/ventas?_start=:page&_limit=10', {}, {
-    return $resource('http://localhost:3000/ventas', {}, {
-    query: {method: 'GET', params: {}, isArray: true}
+//    return $resource('http://localhost:3000/ventas?_start=:page&_limit=10', {},
+    //return $resource('http://localhost:3000/ventas', {}, {
+    return $resource('https://pweb-demo-api.herokuapp.com/api/v1/ventas', {}, {
+    query: {method: 'GET', params: {}, headers: {'Authorization':'Bearer 096fa935862e4c55db73e8f153ef867f'}}
   });
 }]);
